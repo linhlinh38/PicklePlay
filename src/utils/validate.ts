bcf352d0ab1cb0ebf24cd9125bdd66f278797568
linhlinh38 <03linhnguyen@gmail.com>
2024-05-17 21:34:18 +0700

    create account service

---
+++ src/utils/validate.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject } from "zod";
+import Logging from "./Logging";
+
+const validate =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+
+      return next();
+    } catch (err: any) {
+      const error_message = JSON.parse(err.message);
+      Logging.error(error_message);
+      return res.status(400).json({
+        status: "Bad Request!",
+        path: error_message[0].path[1],
+        message: error_message[0].message,
+      });
+    }
+  };
+
+export default validate;
