464caec4a54ede2621019109ff64b9cc65785dcc
linhlinh38 <03linhnguyen@gmail.com>
2024-05-17 13:47:32 +0700

    create account model

---
+++ src/models/validateSchema/createAccount.validate.schema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const createAccountSchema = z.object({
+  body: z.object({
+    username: z
+      .string()
+      .min(1, { message: "Username must be greater than 1 characters!" }),
+    email: z
+      .string()
+      .min(1, { message: "Email must be greater than 1 characters!" }),
+    password: z
+      .string({ description: "Password is required" })
+      .min(8, { message: "Password must be greater than 8 characters!" }),
+    role: z
+      .string()
+      .min(1, { message: "Role must be greater than 1 characters!" })
+      .refine(
+        (value) =>
+          value === "admin" || value === "customer" || value === "manager",
+        {
+          message: "Role must be a valid role!",
+        }
+      ),
+  }),
+});
+
+export type createAccountType = z.infer<typeof createAccountSchema>;
