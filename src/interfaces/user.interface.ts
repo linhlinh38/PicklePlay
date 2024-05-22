1d0c6d8847e4f57c3b44a61316731b3cfc237b5d
linhlinh38 <03linhnguyen@gmail.com>
2024-05-22 23:25:40 +0700

    implement user interface

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+  username: string;
+  email: string;
+  role: string;
+  password: string;
+  gender: string;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  expried_date: string;
+  max_court: number;
+}
