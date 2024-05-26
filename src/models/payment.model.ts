10a331056555a2f7b0f4c06bf2978dd4e7631733
Van Tho Tran <thotv5@fpt.com>
2024-05-26 23:09:33 +0700

    add expDate to payment

--- src/models/payment.model.ts
+++ src/models/payment.model.ts
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 
-const paymentSchema = mongoose.Schema(
-  {
-    accountNumber: {
-      type: String,
-      required: true,
-    },
-    accountName: {
-      type: String,
-      required: true,
-    },
-    accountBank: {
-      type: String,
-    },
-  }
-);
+const paymentSchema = mongoose.Schema({
+  accountNumber: {
+    type: String,
+    required: true,
+  },
+  accountName: {
+    type: String,
+    required: true,
+  },
+  accountBank: {
+    type: String,
+  },
+  expDate: {
+    type: Date,
+  },
+});
 
 const paymentModel = mongoose.model('Payment', paymentSchema);
 export default paymentModel;
