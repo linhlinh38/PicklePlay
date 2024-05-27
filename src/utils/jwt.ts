afacd4a950d0a85f27c0186c0870d28ce66df8c8
linhlinh38 <03linhnguyen@gmail.com>
2024-05-27 16:25:25 +0700

    config for refresh token

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,13 +2,17 @@ import jwt, { VerifyOptions, SignOptions } from "jsonwebtoken";
 // import Roles from "../constant/roles";
 
 import { config } from "../config/envConfig";
-const { secretKey } = config;
+const { SECRET_KEY_FOR_ACCESS_TOKEN, SECRET_KEY_FOR_REFRESH_TOKEN } = config;
 const ACCESS_TOKEN_EXP = "15m";
 export const REFRESH_TOKEN_EXP = "30d";
 
-// export type RefreshToken = {
-//   sessionId: SessionDocument["_id"];
-// };
+export function generateRefreshToken(userId: string) {
+  const payload = { userId: userId };
+  const refreshToken = jwt.sign(payload, SECRET_KEY_FOR_REFRESH_TOKEN, {
+    expiresIn: "7d",
+  });
+  return refreshToken;
+}
 
 export type AccessToken = {
   userId: string;
@@ -21,7 +25,7 @@ export const signToken = (
     secret?: string;
   }
 ) => {
-  const { secret = secretKey, ...opts } = options || {};
+  const { secret = SECRET_KEY_FOR_ACCESS_TOKEN, ...opts } = options || {};
   return jwt.sign(payload, secret, {
     audience: "minh",
     expiresIn: ACCESS_TOKEN_EXP,
@@ -35,7 +39,7 @@ export const verifyToken = <T extends object = AccessToken>(
     secret?: string;
   }
 ) => {
-  const { secret = secretKey, ...opts } = options || {};
+  const { secret = SECRET_KEY_FOR_ACCESS_TOKEN, ...opts } = options || {};
   try {
     const payload = jwt.verify(token, secret, {
       //   audience: ,
