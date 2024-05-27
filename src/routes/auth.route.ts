7e4b1bde4dbd1e92f547fad8ffe5b4a66dbaefc9
linhlinh38 <03linhnguyen@gmail.com>
2024-05-27 16:26:19 +0700

    test refresh token

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import authController from "../controllers/auth.controller";
+import auth from "../middlewares/auth";
 
 const authRoute = express.Router();
 authRoute.post("/login", authController.login);
+authRoute.use(auth);
+authRoute.post("/refresh", authController.refreshToken);
 authRoute.get("/me", authController.getProfile);
 export default authRoute;
