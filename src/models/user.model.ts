497e902ef496c8ae2a6451db071bc2c26b37788a
Van Tho Tran <thotv.t1.1821@gmail.com>
2024-05-30 03:40:31 +0700

    ad

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -53,5 +53,5 @@ const userSchema = mongoose.Schema(
   }
 );
 
-const userModel = mongoose.model('User', userSchema);
+const userModel = mongoose.model('User', userSchema)
 export default userModel;
