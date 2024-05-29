583f0ae00bd7122b329ec151ec02f536dbdad0cb
Van Tho Tran <thotv.t1.1821@gmail.com>
2024-05-30 03:34:58 +0700

    test format code

--- src/models/admin.model.ts
+++ src/models/admin.model.ts
@@ -3,7 +3,7 @@ import userModel from './user.model';
 const mongoose = require('mongoose');
 
 const adminSchema = mongoose.Schema({});
-adminSchema.add(userModel.schema);
+adminSchema.add(userModel.schema)
 
 const adminModel = userModel.discriminator('Admin', adminSchema);
 
