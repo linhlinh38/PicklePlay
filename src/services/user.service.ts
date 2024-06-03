2487146eb1f1fabab64cc4e6b84448ae29759fbe
linhlinh38 <03linhnguyen@gmail.com>
2024-06-03 21:28:12 +0700

    customer service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,18 +1,12 @@
-import { IUser } from '../interfaces/user.interface';
-import userModel from '../models/user.model';
-import { BaseService } from './base.service';
-import bcrypt from 'bcrypt';
+import { IUser } from "../interfaces/user.interface";
+import userModel from "../models/user.model";
+import { BaseService } from "./base.service";
+import bcrypt from "bcrypt";
 
 class UserService extends BaseService<IUser> {
   constructor() {
     super(userModel);
   }
-
-  encryptedPassword = async function (password: string) {
-    const salt = await bcrypt.genSalt(8);
-    const hash = await bcrypt.hash(password, salt);
-    return hash;
-  };
 
   // async getById(id: string): Promise<IUser> {
   //   try {
