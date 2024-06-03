3446823b4d2d70420d416fac2619045e0d363a79
linhlinh38 <03linhnguyen@gmail.com>
2024-06-03 22:05:52 +0700

    update user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,19 +1,13 @@
 import { EmailAlreadyExistError } from '../errors/emailAlreadyExistError';
 import { IUser } from '../interfaces/user.interface';
 import userModel from '../models/user.model';
+import { encryptedPassword } from '../utils/jwt';
 import { BaseService } from './base.service';
-import bcrypt from 'bcrypt';
 
 class UserService extends BaseService<IUser> {
   constructor() {
     super(userModel);
   }
-
-  encryptedPassword = async function (password: string) {
-    const salt = await bcrypt.genSalt(8);
-    const hash = await bcrypt.hash(password, salt);
-    return hash;
-  };
 
   async beforeCreate(data: IUser): Promise<void> {
     const key: Partial<IUser> = { email: data.email };
@@ -21,7 +15,7 @@ class UserService extends BaseService<IUser> {
     if (user.length !== 0) {
       throw new EmailAlreadyExistError();
     }
-    data.password = await userService.encryptedPassword(data.password);
+    data.password = await encryptedPassword(data.password);
   }
 
   async beforeUpdate(id: string, data: IUser): Promise<void> {
