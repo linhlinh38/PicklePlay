62afa9911ceb9901790e24c5154deceb82519452
linhlinh38 <03linhnguyen@gmail.com>
2024-06-04 20:44:19 +0700

    interface for booking and court

---
+++ src/interfaces/booking.interface.ts
@@ -0,0 +1,11 @@
+export interface IBooking {
+  type: string;
+  paymentType: string;
+  paymentMethod: string;
+  totalPrice: number;
+  totalHour: string;
+  startDate: string;
+  endDate: string;
+  status: string;
+  customer: string;
+}
