3eeb9cda837b4b49a657b2ae8b979e045286fd84
linhlinh38 <03linhnguyen@gmail.com>
2024-06-04 20:47:23 +0700

    create court router

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,11 +1,13 @@
-import express from "express";
-import userRoute from "./user.route";
-import authRoute from "./auth.route";
-import customerRoute from "./customer.route";
+import express from 'express';
+import userRoute from './user.route';
+import authRoute from './auth.route';
+import customerRoute from './customer.route';
+import courtRoute from './court.route';
 
 const router = express.Router();
-router.use("/user", userRoute);
-router.use("/auth", authRoute);
-router.use("/customer", customerRoute);
+router.use('/user', userRoute);
+router.use('/auth', authRoute);
+router.use('/customer', customerRoute);
+router.use('/court', courtRoute);
 
 export default router;
