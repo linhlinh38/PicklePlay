c1f9652535096fef17a1ace46375b4e29a23cd55
linhlinh38 <03linhnguyen@gmail.com>
2024-06-11 00:47:27 +0700

    hot fixed schedule

--- src/services/schedule.service.ts
+++ src/services/schedule.service.ts
@@ -7,6 +7,7 @@ import { BookingTypeEnum, ScheduleStatusEnum } from '../utils/enums';
 import { BaseService } from './base.service';
 import { bookingService } from './booking.service';
 import { userService } from './user.service';
+import slotModel from '../models/slot.model';
 
 class ScheduleService extends BaseService<ISchedule> {
   constructor() {
@@ -16,42 +17,62 @@ class ScheduleService extends BaseService<ISchedule> {
   async getSlotDuration(startslot: string, endslot: string): Promise<number> {
     const startTime = moment(startslot, 'HH:mm');
     const endTime = moment(endslot, 'HH:mm');
+
     const duration = moment.duration(endTime.diff(startTime));
     const slotDuration = duration.asHours();
     return slotDuration;
   }
 
   async beforeCreate(data: ISchedule): Promise<void> {
-    const checkSchedule = await scheduleService.search({
+    const checkSchedule = await scheduleModel.find({
       court: data.court,
-      slot: data.slot,
+      slots: { $in: data.slots },
       date: data.date,
       status: ScheduleStatusEnum.AVAILABLE
     });
 
+    if (
+      moment(data.date, 'YYYY-MM-DD').isSame(
+        moment(new Date(), 'YYYY-MM-DD')
+      ) &&
+      moment(new Date(), 'HH:mm').isAfter(moment(data.startTime, 'HH:mm'))
+    ) {
+      throw new BadRequestError(
+        'Schedule cannot create due to unvalid start time'
+      );
+    }
+
     if (checkSchedule.length !== 0) {
       throw new BadRequestError('Schedule is already booking');
     }
-    const booking = await bookingService.getById(data.booking as string);
-    const startslot = '07:00';
-    const endslot = '09:00';
+    let booking;
+    if (data.booking) {
+      booking = await bookingService.getById(data.booking as string);
+      if (booking.type === BookingTypeEnum.FLEXIBLE_SCHEDULE) {
+        const slots = await slotModel.find({ _id: { $in: data.slots } });
+        let bookingHour = 0;
+        await slots.map(async (slot) => {
+          const calulateDuration = await this.getSlotDuration(
+            slot.startTime,
+            slot.endTime
+          );
+          bookingHour += calulateDuration;
+        });
 
-    const bookingHour = await this.getSlotDuration(startslot, endslot);
-
-    if (booking.type === BookingTypeEnum.FLEXIBLE_SCHEDULE) {
-      if (
-        moment(booking.startDate, 'YYYY-MM-DD').isBefore(
-          moment(new Date(), 'YYYY-MM-DD')
-        )
-      ) {
-        throw new BadRequestError(
-          `Schedule cannot create due to Booking start date is ${booking.startDate}`
-        );
-      }
-      if (bookingHour > booking.totalHour) {
-        throw new BadRequestError(
-          `Schedule cannot create due to Booking Flexible only remain ${booking.totalHour} Hour`
-        );
+        if (
+          moment(booking.startDate, 'YYYY-MM-DD').isAfter(
+            moment(new Date(), 'YYYY-MM-DD')
+          )
+        ) {
+          throw new BadRequestError(
+            `Schedule cannot create due to Booking start date is ${booking.startDate}`
+          );
+        }
+        if (bookingHour > booking.totalHour) {
+          throw new BadRequestError(
+            `Schedule cannot create due to Booking Flexible only remain ${booking.totalHour} Hour`
+          );
+        }
       }
     }
   }
@@ -60,17 +81,25 @@ class ScheduleService extends BaseService<ISchedule> {
     await this.beforeCreate(data);
     const newSchedule = new this.model(data);
     await newSchedule.save();
+    let booking;
+    if (newSchedule.booking) {
+      booking = await bookingService.getById(newSchedule.booking as string);
+      if (booking.type === BookingTypeEnum.FLEXIBLE_SCHEDULE) {
+        const slots = await slotModel.find({ _id: { $in: data.slots } });
+        let bookingHour = 0;
+        await slots.map(async (slot) => {
+          const calulateDuration = await this.getSlotDuration(
+            slot.startTime,
+            slot.endTime
+          );
+          bookingHour += calulateDuration;
+        });
 
-    const booking = await bookingService.getById(data.booking as string);
-    const startslot = '07:00';
-    const endslot = '09:00';
-
-    if (booking.type === BookingTypeEnum.FLEXIBLE_SCHEDULE) {
-      const bookingHour = await this.getSlotDuration(startslot, endslot);
-      await bookingService.updateTotalHours(
-        data.booking as string,
-        bookingHour
-      );
+        await bookingService.updateTotalHours(
+          data.booking as string,
+          bookingHour
+        );
+      }
     }
 
     return newSchedule;
