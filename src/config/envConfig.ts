18c2048c02f07e94324e0a1af5f7f3954ac2c06c
linhlinh38 <03linhnguyen@gmail.com>
2024-06-19 13:08:34 +0700

    mail setting

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import { MailerSend } from 'mailersend';
 
 dotenv.config();
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT;
 const SECRET_KEY_FOR_ACCESS_TOKEN = process.env.SECRET_KEY_FOR_ACCESS_TOKEN;
 const SECRET_KEY_FOR_REFRESH_TOKEN = process.env.SECRET_KEY_FOR_REFRESH_TOKEN;
 const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+const MAILERSEND_API_KEY = process.env.MAILERSEND_API_KEY;
 export const config = {
   mongo_uri: MONGO_DB_URI,
   port: PORT,
@@ -14,3 +16,7 @@ export const config = {
   SECRET_KEY_FOR_REFRESH_TOKEN: SECRET_KEY_FOR_REFRESH_TOKEN,
   FIREBASE_PROJECT_ID: FIREBASE_PROJECT_ID
 };
+
+export const mailersend = new MailerSend({
+  apiKey: MAILERSEND_API_KEY
+});
