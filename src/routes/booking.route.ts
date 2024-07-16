dbdb2ccb8d309d92ca7928fb7453800c9ab74897
linhlinh38 <03linhnguyen@gmail.com>
2024-07-16 21:31:58 +0700

    update schedule route

--- src/routes/booking.route.ts
+++ src/routes/booking.route.ts
@@ -10,10 +10,15 @@ const bookingRouter = express.Router();
 bookingRouter.use(authentication);
 bookingRouter.post(
   '/',
+  Author([RoleEnum.CUSTOMER]),
   validate(createBookingSchema),
   bookingController.createBooking
 );
-bookingRouter.post('/competition', bookingController.createCompetionBooking);
+bookingRouter.post(
+  '/competition',
+  Author([RoleEnum.CUSTOMER]),
+  bookingController.createCompetionBooking
+);
 bookingRouter.get('/', bookingController.getAllBooking);
 bookingRouter.get(
   '/GetAllBookingOfCourt/:court',
@@ -40,7 +45,11 @@ bookingRouter.get(
 );
 bookingRouter.get('/:id', bookingController.getBookingById);
 bookingRouter.post('/cancel/:id', bookingController.cancelBooking);
-bookingRouter.put('/done/:id', bookingController.doneBooking);
+bookingRouter.put(
+  '/done/:id',
+  Author([RoleEnum.MANAGER, RoleEnum.STAFF]),
+  bookingController.doneBooking
+);
 bookingRouter.post('/search', bookingController.searchBooking);
 
 export default bookingRouter;
